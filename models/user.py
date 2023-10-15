0ceafd148c15310dbaec0a3df3ca791bd75a769a
MustaSteve <avineusq@gmail.com>
2023-10-15 11:17:57 -0700

    updated

--- models/user.py
+++ models/user.py
@@ -1,14 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-"""
-class User that inherits fro BaseModel
-"""
-from models.basemodel import BaseModel
-
-
-class User(BaseModel):
-    """all public class attributes"""
-=======
 ''' the user class module'''
 
 from models.base_model import BaseModel
@@ -18,7 +8,6 @@ from models.base_model import BaseModel
 class User(BaseModel):
     '''user class'''
 
->>>>>>> ed407161745b016a92e3b168f5cff3cf6c9ebef6
     email = ""
     password = ""
     first_name = ""
