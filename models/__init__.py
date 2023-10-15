86f96cf950407f90c24a5b1cb9039443de2a6183
MustaSteve <avineusq@gmail.com>
2023-10-15 11:22:37 -0700

    updated

--- models/__init__.py
+++ models/__init__.py
@@ -1,19 +1,7 @@
-<<<<<<< HEAD
-
-=======
-<<<<<<< HEAD
 """Module for FileStorage autoinit."""
 
 from models.engine.file_storage import FileStorage
 '''FileStorage module'''
 
 storage = FileStorage()
-=======
-#!/usr/bin/python3
-import models.engine.file_storage
-
-
-storage = models.engine.file.FileStorage()
->>>>>>> cee218873ce7e8d05ee9bb698be63f21f8810120
 storage.reload()
->>>>>>> ed407161745b016a92e3b168f5cff3cf6c9ebef6
